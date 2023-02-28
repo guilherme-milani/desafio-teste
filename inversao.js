0c7e699e4f43d076eb065e032dcc359158e59ed1
guilherme-milani <124526385+guilherme-milani@users.noreply.github.com>
2023-02-28 13:50:47 -0300

    inversao.js

---
+++ inversao.js
@@ -0,0 +1,8 @@
+let desafioTeste = "Esta é uma string de teste";
+let desafioTesteInvertido = "";
+
+for (let i = desafioTeste.length - 1; i >= 0; i--) {
+  desafioTesteInvertido += desafioTeste[i];
+}
+
+console.log(desafioTesteInvertido);
